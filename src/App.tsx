2eec58dc120ac784252e290fc533cd70a5eb93b6
JavaScriptN0ob <wenpeijs@gmail.com>
2021-09-11 23:18:40 +1000

    added new state and props type witg interface to app.tsx, also added fetch api where from json placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,49 @@ import robotsData from './mock/robots.json';
 import Robots from './components/Robot';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>Online Shopping</h1>
+interface Props {
+
+}
+
+interface State {
+  robotGallery: any[],
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      robotGallery: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    const { robotGallery } = this.state;
+
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>Online Shopping</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {robotGallery.map(({
+            id,
+            name,
+            email,
+          }) => <Robots key={id} id={id} name={name} email={email} />)}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotsData.map(({
-          id,
-          name,
-          email,
-        }) => <Robots key={id} id={id} name={name} email={email} />)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
