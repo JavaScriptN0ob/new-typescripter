e251dccdc6b54d648a93cf84091c255c1bd4f5df
JavaScriptN0ob <wenpeijs@gmail.com>
2021-09-12 11:21:37 +1000

    refactor promise with async/await

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import robotsData from './mock/robots.json';
 import Robots from './components/Robot';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
-interface Props {
-  props: any;
-}
-
-const App: React.FC<Props> = (props) => {
+const App: React.FC = (props) => {
   const [robotGallery, setRobotGallery] = useState<any>([]);
-  
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((data) => setRobotGallery(data))
+    const getRobotGallery = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json();
+  
+      setRobotGallery(data);
+    }
+    
+    getRobotGallery();
   }, []);
 
 
