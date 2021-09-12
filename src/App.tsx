3f9f0eb372866bbba5a350c9004be72d573f59d2
JavaScriptN0ob <wenpeijs@gmail.com>
2021-09-12 12:35:01 +1000

    added error message

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,29 @@ import robotsData from './mock/robots.json';
 import Robots from './components/Robot';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
+// interface State {
+//   robotGallery: any[];
+//   loading: boolean;
+// }
+
 const App: React.FC = (props) => {
   const [robotGallery, setRobotGallery] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     const getRobotGallery = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      const data = await response.json();
-  
-      setRobotGallery(data);
+      setLoading(true);
+
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json();
+    
+        setRobotGallery(data);
+      } catch (error: any) {
+        setError(error.message);
+      }
+      setLoading(false);
     }
     
     getRobotGallery();
@@ -27,13 +41,19 @@ const App: React.FC = (props) => {
         <h1>Online Shopping</h1>
       </div>
       <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotGallery.map(({
-          id,
-          name,
-          email,
-        }) => <Robots key={id} id={id} name={name} email={email} />)}
-      </div>
+      {(!error || error !== '') && <div>Error occurs: {error}</div>}
+      {!loading ? (
+        <div className={styles.robotList}>
+          {robotGallery.map(({
+            id,
+            name,
+            email,
+          }) => <Robots key={id} id={id} name={name} email={email} />)}
+        </div>
+      ) : (
+        <h2>loading</h2>
+      )
+      }
     </div>
   );
 }
