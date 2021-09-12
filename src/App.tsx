e46f032435f32a1c88c11e879747bd5bf2c06c80
JavaScriptN0ob <wenpeijs@gmail.com>
2021-09-12 11:13:56 +1000

    rebuilt class with func component by hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo.svg';
 import styles from './App.module.css';
 import robotsData from './mock/robots.json';
@@ -6,48 +6,35 @@ import Robots from './components/Robot';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 interface Props {
-
+  props: any;
 }
 
-interface State {
-  robotGallery: any[],
-}
-
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      robotGallery: [],
-    }
-  }
-
-  componentDidMount() {
+const App: React.FC<Props> = (props) => {
+  const [robotGallery, setRobotGallery] = useState<any>([]);
+  
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
-    .then((data) => this.setState({ robotGallery: data }));
-  }
+    .then((data) => setRobotGallery(data))
+  }, []);
 
-  render() {
-    const { robotGallery } = this.state;
 
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>Online Shopping</h1>
-        </div>
-        <ShoppingCart />
-        <div className={styles.robotList}>
-          {robotGallery.map(({
-            id,
-            name,
-            email,
-          }) => <Robots key={id} id={id} name={name} email={email} />)}
-        </div>
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>Online Shopping</h1>
       </div>
-    );
-  }
+      <ShoppingCart />
+      <div className={styles.robotList}>
+        {robotGallery.map(({
+          id,
+          name,
+          email,
+        }) => <Robots key={id} id={id} name={name} email={email} />)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
